6a7d752f6e083cdd1de9f7674bf0b4e5983a611b
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-11-02 10:09:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,25 @@
+from flask import Flask, url_for, render_template, request, Markup, flash
+import os
 import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return get_state_options()
+  
+def get_state_options():
+    with open('county_demographics.json') as demographics_data:
+      counties = json.load(demographics_data)
+      
+    options = ""
+    state = ""
+    for c in counties:
+        if not c["State"] == state:
+          options += Markup("<option value=/"" + c["State"] + "\">" + c["State"] + "</options")
+          state = c["State"]
+          
+    return render_template('home.html', options = options)
+  
+if __name__=="__main__":
+  app.run(debug=False, port=54321)
