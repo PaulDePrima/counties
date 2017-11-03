b8e02c75fa4d59955551ef5d1984f99e76a66818
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-11-03 09:02:54 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,22 +4,41 @@ import json
 
 app = Flask(__name__)
 
+with open('county_demographics.json') as demographics_data:
+       counties = json.load(demographics_data)
+
 @app.route("/")
 def render_main():
-    return get_state_options()
-  
+    return render_template('home.html', options = get_state_options())
+
+@app.route("/home")
+def render_response():
+    state = request.args["state"]
+    return render_template('home.html', options = get_state_options(),response = your_interesting_demographic_function(state))
+
 def get_state_options():
-    with open('county_demographics.json') as demographics_data:
-      counties = json.load(demographics_data)
-      
+        
     options = ""
     state = ""
     for c in counties:
         if not c["State"] == state:
-          options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
-          state = c["State"]
-          
-    return render_template('home.html', options = options)
-  
+            options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+            state = c["State"]
+    
+    return options
+
+def your_interesting_demographic_function(stateName):
+    count = 0
+    while not counties[count]["State"] == stateName:
+        count += 1
+           
+    state = counties[count]["State"] 
+    county = counties[count]["County"]
+    word = "Employment: Private Non-farm Establisment: "
+    employ = counties[count]["Employment"]["Private Non-farm Establishments"]
+    return state + ": " + county + ": " + word + str(employ)
+
+
 if __name__=="__main__":
-  app.run(debug=False, port=54321)
+    app.run(debug=False, port=54321)
+
